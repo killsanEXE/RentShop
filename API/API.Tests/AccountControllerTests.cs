c6e18d535b38f1dc95c40497e0758fa52d4bf9e6
killsanexe <killsan.exe@gmail.com>
2022-04-27 08:50:32 +0300

    connected unit tests

--- API/API.Tests/AccountControllerTests.cs
+++ API/API.Tests/AccountControllerTests.cs
@@ -2,17 +2,16 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
-using API.Controllers;
 using Xunit;
-using FakeItEasy;
 
 namespace API.Tests
 {
     public class AccountControllerTests
     {
         [Fact]
-        public void GetRegisterReturn200()
+        public void RegisterReturn200()
         {
+            Assert.Equal(1, 1);
         }
     }
 }
